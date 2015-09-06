794969138ed18b0f796e4a0f39a886a6f6a2249f
rowanv <rrvspam@gmail.com>
2015-09-06 10:47:20 -0500

    Refactor list view to handle new item POSTs.

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -10,7 +10,7 @@ class ItemValidationTest(FunctionalTest):
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys('\n')
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
@@ -18,21 +18,20 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         #She tries again with some text for the item, which now works
-        inputbox.send_keys('Get milk')
-        inputbox.send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table('1: Get milk')
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         #Pervsersely, she now decides to submit a second blank list item
-        inputbox.send_keys(Keys.ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # She receives a similar warning on the list page
-        self.check_for_row_in_list_table('1: Get milk')
+        self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
         #And she can correct it by filling some text in
-        inputbox.send_keys('Make tea')
-        self.check_for_row_in_list_table('1: Get milk')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
 
 
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -11,7 +11,7 @@ def new_list(request):
     item = Item.objects.create(text=request.POST['item_text'], list=list_)
     try:
     	item.full_clean()
-    	item.save()
+    	item.save() #do we need this?
     except ValidationError:
     	list_.delete()
     	error = "You can't have an empty list item"
@@ -19,13 +19,12 @@ def new_list(request):
     return redirect('/lists/%d/' % (list_.id,))
 
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d' % (list_.id,))
     return render(request, 'list.html', {'list': list_})
 
